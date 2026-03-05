3af5a5a32775d710af69bc14d8bd092835ead803
kandpalharsha <kandpalharsha15@gmail.com>
2026-03-05 20:04:58 +0530

    Add environment variable support for API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,10 +1,12 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
 export const analyzeResume = async (resume, jobText) => {
   const formData = new FormData();
 
   formData.append("resume", resume);
   formData.append("job_description", jobText);
 
-  const res = await fetch("http://localhost:5000/analyze", {
+  const res = await fetch(`${API_URL}/analyze`, {
     method: "POST",
     body: formData,
   });
